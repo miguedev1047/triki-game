01ba1fcd7cb31fe2a65177be2220d4edb705497c
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-08-02 11:18:11 -0500

    FEAT: add check winner utillity ⚒️

---
+++ apps/web/src/utils/board.ts
@@ -0,0 +1,21 @@
+import type { GameBoard } from '@/types'
+import { WINNER_COMBOS } from '@/constants'
+
+export function checkWinner(boardToCheck: GameBoard) {
+  for (const combos of WINNER_COMBOS) {
+    const [a, b, c] = combos
+    if (
+      boardToCheck[a] &&
+      boardToCheck[a] === boardToCheck[b] &&
+      boardToCheck[a] === boardToCheck[c]
+    ) {
+      return boardToCheck[a]
+    }
+  }
+  return null
+}
+
+export function checkIsDraw(boardToCheck: GameBoard) {
+  const isDraw = boardToCheck.every((square) => square !== null)
+  return isDraw
+}
