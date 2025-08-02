50ecc3d19b012f6d5d8c03715786f57f6ea469ec
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-08-02 11:22:02 -0500

    FEAT: add fade state hook ⚒️

---
+++ apps/web/src/hooks/fade-state.ts
@@ -0,0 +1,47 @@
+import type { Cell, FadeState } from '@/types'
+import { useGame } from '@/store/use-game'
+import { useEffect, useState } from 'react'
+import { PLAYS_TO_REMOVE } from '@/constants'
+
+export function useFadeState(index: number, symbol: Cell, history: Cell[]) {
+  const [fadeState, setFadeState] = useState<FadeState>('FULL')
+  const winnerStatus = useGame((s) => s.winner)
+
+  useEffect(() => {
+    if (winnerStatus !== 'NONE') {
+      setFadeState('FULL')
+      return
+    }
+
+    if (!symbol) {
+      setFadeState('FULL')
+      return
+    }
+
+    const symbolPositionInHistory = history.findIndex(
+      (cellIndex) => cellIndex === (index as never)
+    )
+
+    if (symbolPositionInHistory === -1) {
+      setFadeState('FULL')
+      return
+    }
+
+    const isAboutToBeRemoved =
+      symbolPositionInHistory === 0 && history.length >= PLAYS_TO_REMOVE
+    const isNextToBeRemoved =
+      symbolPositionInHistory === 1 && history.length >= PLAYS_TO_REMOVE
+    const willBeRemovedSoon =
+      symbolPositionInHistory === 0 && history.length === PLAYS_TO_REMOVE - 1
+
+    if (isAboutToBeRemoved) {
+      setFadeState('FADING')
+    } else if (isNextToBeRemoved || willBeRemovedSoon) {
+      setFadeState('INTERMEDIATE')
+    } else {
+      setFadeState('FULL')
+    }
+  }, [history, symbol, index, winnerStatus])
+
+  return fadeState
+}
