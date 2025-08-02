bb7fa7ebb8b5ce33ff03e7dbf5547f5c2c1f9069
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-08-02 11:23:32 -0500

    FEAT: add sound effects 🎵

---
+++ apps/web/src/assets/audio-effects/_index.ts
@@ -0,0 +1,5 @@
+import TapSound from '@/assets/audio-effects/tap-sound.wav'
+import WinSound from '@/assets/audio-effects/win-sound.wav'
+import DrawSound from '@/assets/audio-effects/draw-sound.wav'
+
+export { TapSound, DrawSound, WinSound }
