b12c8ddd89737b51dd8f2c102d2bcfd24e1efa76
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-08-03 06:48:43 -0500

    STYLE: update width 🎨

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -21,7 +21,7 @@ function HomeComponent() {
         <Box className='bg-primary flex flex-col gap-4 z-50 p-5'>
           <BoardHeader />
 
-          <Box className='w-[340px] sm:w-[480px] md:w-[640px] h-auto grid relative grid-cols-3 gap-4 md:gap-8 p-4 md:p-8'>
+          <Box className='w-[320px] sm:w-[480px] md:w-[640px] h-auto grid relative grid-cols-3 gap-4 md:gap-8 p-4 md:p-8'>
             {board.map((_, index) => (
               <Square
                 index={index}
