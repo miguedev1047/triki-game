55480adc495cf7a0cb41e7409fb76a415584adc9
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-08-03 06:35:57 -0500

    FEAT: add board header component 🎨

---
+++ apps/web/src/components/board-header.tsx
@@ -0,0 +1,22 @@
+import { useGame } from '@/store/use-game'
+import { Box } from '@/components/ui/8bit/box'
+import { Button } from '@/components/ui/8bit/button'
+import { ReloadIcon } from '@/assets/icons/reload-icon'
+
+export function BoardHeader() {
+  const resetGame = useGame((s) => s.resetGame)
+
+  return (
+    <div className='absolute w-full top-0 -translate-y-[calc(100%_+_24px)] '>
+      <Box className='flex flex-1 items-end justify-between '>
+       <h2 className='md:text-2xl'>Triki Game</h2>
+        <Button
+          onClick={resetGame}
+          size='icon'
+        >
+          <ReloadIcon />
+        </Button>
+      </Box>
+    </div>
+  )
+}
