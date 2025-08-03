6286348dd83b27a95f9f7e50a521412b7972d5f8
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-08-03 06:51:03 -0500

    CHORE: minor changes 🎨

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -14,11 +14,11 @@ function HomeComponent() {
   const board = useGame((s) => s.board)
 
   return (
-    <main className='p-4 md:p-8 flex items-center justify-center w-full h-svh'>
+    <main className='p-2 flex items-center justify-center w-full h-svh'>
       <Box className='size-full flex items-center justify-center p-4 md:p-8'>
         <ModeToggle />
 
-        <Box className='bg-primary flex flex-col gap-4 z-50 p-5'>
+        <div className='flex flex-col gap-4 z-50 p-2'>
           <BoardHeader />
 
           <Box className='w-[320px] sm:w-[480px] md:w-[640px] h-auto grid relative grid-cols-3 gap-4 md:gap-8 p-4 md:p-8'>
@@ -29,7 +29,7 @@ function HomeComponent() {
               />
             ))}
           </Box>
-        </Box>
+        </div>
 
         <GameResultDialog />
       </Box>
