7581a4f0ef0130987bce12523c6b7698607c3c47
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-08-03 06:36:35 -0500

    FEAT: add reload icon 🎨

---
+++ apps/web/src/assets/icons/reload-icon.tsx
@@ -0,0 +1,20 @@
+import type { SVGProps } from 'react'
+
+export function ReloadIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='1em'
+      height='1em'
+      preserveAspectRatio='xMidYMid'
+      viewBox='0 0 30 30'
+      aria-hidden='true'
+      {...props}
+    >
+      <path
+        color='currentColor'
+        d='M 6 4 L 6 6 L 4 6 L 4 8 L 2 8 L 2 10 L 6 10 L 6 26 L 17 26 L 17 24 L 8 24 L 8 10 L 12 10 L 12 8 L 10 8 L 10 6 L 8 6 L 8 4 L 6 4 z M 15 6 L 15 8 L 24 8 L 24 22 L 20 22 L 20 24 L 22 24 L 22 26 L 24 26 L 24 28 L 26 28 L 26 26 L 28 26 L 28 24 L 30 24 L 30 22 L 26 22 L 26 6 L 15 6 z'
+      />
+    </svg>
+  )
+}
